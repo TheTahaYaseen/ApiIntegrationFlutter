7a991446adf0d011b85b1cdf61a4d12cf7c7f117
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-06 16:42:23 +0500

    Registration Backend Made

--- lib/services.dart
+++ lib/services.dart
@@ -1,1 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+registerUser({required String name, required String email}) async {
+  var apiUrl = "https://mhuzaifakhan.pythonanywhere.com/users";
+  var api = Uri.parse(apiUrl);
+  final response = await http.post(api,
+      headers: <String, String>{"Content-Type": "application/json"},
+      body: jsonEncode({"name": name, "email": email}));
+
+  if (response.statusCode == 200) {
+    return "Successfully Registered!";
+    return "Successfully Registered!";
+  } else {
+    return "An Error Occured During Registration! Please Try Again!";
+  }
+}
