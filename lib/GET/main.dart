6e3047372cc20ca165da782cfbc08f90d9fb4aac
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-06 16:15:34 +0500

    Moved Into GET

---
+++ lib/GET/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'API Integration',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder(
+      future: getUsers(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data![index].name!),
+                subtitle: Text(snapshot.data![index].email!),
+              );
+            },
+          );
+        }
+        return const CircularProgressIndicator();
+      },
+    ));
+  }
+}
